c2d23272229d3274813ee4e5c7d1fd39a13924d8
Tarik Jaber <tariknjaber@gmail.com>
2024-09-17 17:28:31 -0400

    Make work

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Network.HTTP.Conduit
 import Data.Aeson
 import GHC.Generics
+import Control.Monad
 
 data Joke = Joke {
     joke :: !String,
@@ -12,12 +13,22 @@ data Joke = Joke {
 
 instance FromJSON Joke
 
-main :: IO()
-main = do
+fetchJoke :: IO()
+fetchJoke = do
     result <- simpleHttp "https://www.yomama-jokes.com/api/v1/jokes/random/"
     case (decode result :: Maybe Joke) of
         Just x -> do
             putStrLn (joke x)
         Nothing -> putStrLn "Error"
+
+loop :: IO()
+loop = do
+    command <- getChar
+    unless (command == 'q') $ do
+        fetchJoke
+        loop
+
+main :: IO()
+main = loop
 
 
